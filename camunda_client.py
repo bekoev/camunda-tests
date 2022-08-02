b249e05a2f5ca1ea007ba6e3489ae20ccb4f490b
Aleksei Bekoev <bekoev.services@gmail.com>
2022-08-02 16:43:17 +0300

    use the pyzeebe lib (async)

--- camunda_client.py
+++ camunda_client.py
@@ -1,40 +1,32 @@
 import asyncio
-import grpc.aio
-import json
-from zeebe_grpc import gateway_pb2
-from zeebe_grpc import gateway_pb2_grpc
+from typing import Dict
+
+from pyzeebe import Job
+from pyzeebe import ZeebeWorker
+from pyzeebe import create_insecure_channel
 
 
-async def run():
-    async with grpc.aio.insecure_channel('localhost:26500') as channel:
-        gateway = gateway_pb2_grpc.GatewayStub(channel)
-        topologyResponse = await gateway.Topology(gateway_pb2.TopologyRequest())  # noqa
-        print(f'Zeebe gateway topology response:\n{topologyResponse}')
+grpc_channel = create_insecure_channel(hostname='localhost', port=26500)
+worker = ZeebeWorker(grpc_channel)
 
-        print('Looking for some "echo" jobs...')
-        while True:
-            activate_jobs_response = gateway.ActivateJobs(
-                gateway_pb2.ActivateJobsRequest(
-                    type="echo",
-                    worker="Python worker",
-                    timeout=60_000,
-                    maxJobsToActivate=32,
-                    requestTimeout=60_000,  # Enable long polling
-                )
-            )
-            async for response in activate_jobs_response:
-                for job in response.jobs:
-                    try:
-                        print(f'Got a job with ID={job.elementId}')
-                        await gateway.CompleteJob(
-                            gateway_pb2.CompleteJobRequest(
-                                jobKey=job.key,
-                                variables=json.dumps({})))
-                        print("Job Completed")
-                    except Exception as e:
-                        await gateway.FailJob(gateway_pb2.FailJobRequest(jobKey=job.key))  # noqa
-                        print(f"Job Failed {e}")
+
+async def print_job_id(job: Job) -> Job:
+    print(f'Got a job with ID={job.element_id}')
+    return job
+
+
+async def print_completed(job: Job) -> Job:
+    print(f'Job Completed')
+    return job
+
+
+@worker.task(task_type='echo', before=[print_job_id], after=[print_completed])
+async def example_task() -> Dict:
+    await asyncio.sleep(2)
+    return {}
 
 
 if __name__ == '__main__':
-    asyncio.run(run())
+    # Workaround from https://github.com/camunda-community-hub/pyzeebe/issues/239
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(worker.work())
